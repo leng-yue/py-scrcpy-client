1f91abc1b75a18dc44e4955a73eba5d13e3166ab
lengyue <lengyue@lengyue.me>
2021-11-22 11:11:18 -0500

    rename folder

--- scripts/lint.py
+++ scripts/lint.py
@@ -1,5 +1,5 @@
 import subprocess
 
-source_dirs = "scrcpy tests script scrcpy_ui"
+source_dirs = "scrcpy tests scripts scrcpy_ui"
 subprocess.check_call(f"isort {source_dirs}", shell=True)
 subprocess.check_call(f"black {source_dirs}", shell=True)
