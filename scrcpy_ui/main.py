e2c22abdd1675151e16074e192604d1e8dfdded5
Pengxiang Gu <gupengxiang@rct.ai>
2022-03-28 17:54:51 +0800

    fix muti device control

--- scrcpy_ui/main.py
+++ scrcpy_ui/main.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 from adbutils import adb
 from PySide6 import QtCore  # QTranslator
+from PySide6.QtCore import Signal
 from PySide6.QtGui import QImage, QMouseEvent, QPixmap, QCloseEvent
 from PySide6.QtWidgets import (
     QAbstractItemView,
@@ -26,6 +27,8 @@ app = QApplication([])
 
 
 class ScreenWindow(QDialog):
+    signal_frame = Signal(np.ndarray)
+    
     def __init__(self, name="", serial_no="", *args, **kwargs):
         super().__init__(*args, **kwargs)
         if not serial_no:
@@ -34,24 +37,23 @@ class ScreenWindow(QDialog):
         self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)  # 始终最前显示
         self.ui.setupUi(self)
 
-        self.alive = True
-        self.max_width = 600
+        self.max_width = 640
         self.serial_no = serial_no
-        self.device = adb.device(serial=serial_no)
-        self.client = scrcpy.Client(
-            device=self.device,
-            bitrate=10000000,
-            max_width=640,
-        )
-        # show
-        self.client.add_listener(scrcpy.EVENT_FRAME, self.on_frame)
+        # # show
+        # self.client.add_listener(scrcpy.EVENT_FRAME, self.on_frame)
+
+        # show 
+        
+        self.signal_frame.connect(self.on_frame)
         # Bind mouse event
         self.ui.label_video.mousePressEvent = self.on_mouse_event(scrcpy.ACTION_DOWN)
         self.ui.label_video.mouseMoveEvent = self.on_mouse_event(scrcpy.ACTION_MOVE)
         self.ui.label_video.mouseReleaseEvent = self.on_mouse_event(scrcpy.ACTION_UP)
 
         self.setWindowTitle(QtCore.QCoreApplication.translate("Dialog", name, None))
+        self.tworker = ThreadWorker(0, self.serial_no, self.signal_frame)
         self.show()
+        
 
     def on_frame(self, frame):
         app.processEvents()
@@ -75,8 +77,8 @@ class ScreenWindow(QDialog):
             focused_widget = QApplication.focusWidget()
             if focused_widget is not None:
                 focused_widget.clearFocus()
-            ratio = self.max_width / max(self.client.resolution)
-            self.client.control.touch(
+            ratio = self.max_width / max(self.tworker.client.resolution)
+            self.tworker.client.control.touch(
                 evt.position().x() - (self.ui.label_video.geometry().x() / 2) / ratio,
                 evt.position().y() - (self.ui.label_video.geometry().y() / 2) / ratio,
                 action,
@@ -86,8 +88,7 @@ class ScreenWindow(QDialog):
 
     def closeEvent(self, _):
         print("close~~~~")
-        self.client.stop()
-        self.alive = False
+        self.tworker.stop()
 
     # def showWindow(self):
     #     print("移动", self.x(), self.y())
@@ -193,7 +194,7 @@ class MainWindow(QMainWindow):
         if not win_screen:
             _win_screen = ScreenWindow(name, serial_no)
             self.dict_screen[serial_no] = _win_screen
-            self.dict_screen[serial_no].client.start()
+            self.dict_screen[serial_no].tworker.start()
             # self.dict_screen[serial_no].showWindow()
         else:
             win_screen.close()
--- scrcpy_ui/worker.py
+++ scrcpy_ui/worker.py
@@ -11,16 +11,17 @@ from scrcpy import MutiClient
 
 
 class ThreadWorker(threading.Thread):  # 继承父类threading.Thread
-    def __init__(self, threadID, serialno):
+    def __init__(self, threadID, serialno, signel=None):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.stop_flag = False
+        self.signel = signel
 
-        self.max_block_frame = 10
+        self.max_block_frame = 100
         self.time_clean_block_list = 10 # s
         self.list_block_frame_time = []
         self.device = adb.device(serial=serialno)
-        self.client = MutiClient(device=self.device, block_frame=False)
+        self.client = MutiClient(device=self.device, block_frame=False, max_width=640)
 
     def run(self):
         for frame in self.client.start():
@@ -28,6 +29,8 @@ class ThreadWorker(threading.Thread):  # 继承父类threading.Thread
                 return
             if isinstance(frame, np.ndarray):
                 print(frame.shape, "TODO -> Need Server.")
+                if self.signel:
+                    self.signel.emit(frame)
                 pass
             else:
                 now = time.time()
